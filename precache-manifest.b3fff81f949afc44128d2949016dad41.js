c423e9d6bac6085a42225b546166cd39bd7a39f5
Piotr Dyrda <piotr.dyrda88@gmail.com>
2021-05-02 21:53:24 +0200

    Updates

---
+++ precache-manifest.b3fff81f949afc44128d2949016dad41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd2a5c7145c1ee0ad7adb3e5d590453",
+    "url": "/servers-manager/index.html"
+  },
+  {
+    "revision": "2093823a99f5dc624218",
+    "url": "/servers-manager/static/css/main.112df126.chunk.css"
+  },
+  {
+    "revision": "91bef189e7f3a1031f72",
+    "url": "/servers-manager/static/js/2.1b28dcef.chunk.js"
+  },
+  {
+    "revision": "2093823a99f5dc624218",
+    "url": "/servers-manager/static/js/main.3f423cf2.chunk.js"
+  },
+  {
+    "revision": "433af2970f02b828237e",
+    "url": "/servers-manager/static/js/runtime~main.7d60e087.js"
+  }
+]);
